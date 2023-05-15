736f6d70bc3abecf52153c6d4e859b5b1bae0570
Nate <nate.esnault@veriphi.co.nz>
2023-05-15 12:46:46 +1200

    Removed old code

--- DualAS6221_BasicReadings/General.h
+++ DualAS6221_BasicReadings/General.h
@@ -46,27 +46,8 @@ volatile boolean readSensor = 0;
 long int t_start = millis();
 
 
-// -------------------------- Global variables  --------------------------
-//AS6212 sensor1;
-//AS6212 sensor2;
-//
-//
-//uint8_t isRespondingSensor1 = S_ALIVE; // 0 = can't establish connection with sensor, 1 = connection ok
-//uint8_t isRespondingSensor2 = S_ALIVE;
-//
-//float diffSensor1_mC = 0.0; // differerence between 2 iterations in milliC
-//float diffSensor2_mC = 0.0; // differerence between 2 iterations in milliC
-//
-//float oldSensor1 = 0.0;
-//float oldSensor2 = 0.0;
-//
-//uint8_t globalTrendSensor1  = T_UNDEF; // 0 = sensed temperature (after moving average) is stable, 1 = decrease, 2 = increase
-//uint8_t globalTrendSensor2  = T_UNDEF;
-
-
 // -------------------------- Function declaration  --------------------------
 void i2cQuickScan                   (void);
-//void sendDataSerial                 (float raw_1, float raw_2);
 void sendDataSerial                 (void);
 void prepareHWTimerInterrupt        (void);
 void checkSensors                   (void);
